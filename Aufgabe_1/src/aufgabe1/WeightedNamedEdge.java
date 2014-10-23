9d896a8cb53c811662c5129f7af3050a2ed85c0a
kumpelblase2 <tim@infinityblade.de>
2014-10-23 07:35:24 +0200

    Added equals and hashcode to edge.

--- Aufgabe_1/src/aufgabe1/WeightedNamedEdge.java
+++ Aufgabe_1/src/aufgabe1/WeightedNamedEdge.java
@@ -63,13 +63,41 @@ public class WeightedNamedEdge extends DefaultEdge
 	//toString()
 	public String toString()
 	{
-		String returnName = "";
+		String returnName = (isDirected() ? "->" : "--");
 		if(getName() != null)
-			returnName += (isDirected() ? "->" : "--") + " " + getName();
+			returnName += " " + getName();
 
 		if(hasWeigth())
-			returnName += (returnName.length() > 0 ? " : " : "") + getWeigth();
+			returnName += " : " + getWeigth();
 
 		return returnName;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+
+		WeightedNamedEdge that = (WeightedNamedEdge)o;
+
+		if(m_directed != that.m_directed) return false;
+		if(m_weigth != that.m_weigth) return false;
+		if(m_name != null ? !m_name.equals(that.m_name) : that.m_name != null) return false;
+		if(!m_source.equals(that.m_source)) return false;
+		if(!m_target.equals(that.m_target)) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int result = m_source.hashCode();
+		result = 31 * result + m_target.hashCode();
+		result = 31 * result + m_weigth;
+		result = 31 * result + (m_name != null ? m_name.hashCode() : 0);
+		result = 31 * result + (m_directed ? 1 : 0);
+		return result;
 	}
 }
