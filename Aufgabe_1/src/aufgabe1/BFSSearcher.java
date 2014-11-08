837e634ca0f64aa336cd1d22cb0c9130ddb2d9d1
Tim Hagemann <tim@infinityblade.de>
2014-11-08 17:15:07 +0100

    Add actual BFS Search

---
+++ Aufgabe_1/src/aufgabe1/BFSSearcher.java
@@ -0,0 +1,69 @@
+package aufgabe1;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import org.jgrapht.Graph;
+
+public class BFSSearcher implements SearchAlgorithm
+{
+	@Override
+	public Path search(final Graph<String, WeightedNamedEdge> inGraph, final String inStart, final String inEnd)
+	{
+		Path path = new Path();
+		if(!inGraph.containsVertex(inStart) || !inGraph.containsVertex(inEnd))
+			return path;
+
+		Queue<String> queue = new ConcurrentLinkedQueue<>();
+		Map<String, VisitedNode> visited = new HashMap<>();
+		VisitedNode startVisited = new VisitedNode();
+		startVisited.name = inStart;
+		startVisited.parent = null;
+		startVisited.value = 0;
+		visited.put(inStart, startVisited);
+		queue.offer(inStart);
+		while(!queue.isEmpty())
+		{
+			String current = queue.poll();
+			VisitedNode node = visited.get(current);
+			path.setSteps(path.getSteps() + 1);
+			for(WeightedNamedEdge edge : inGraph.edgesOf(current))
+			{
+				String target = edge.getTarget();
+				int value = node.value + 1;
+				if(!visited.containsKey(target))
+				{
+					VisitedNode newVisited = new VisitedNode();
+					newVisited.name = target;
+					newVisited.parent = current;
+					newVisited.value = value;
+					visited.put(target, newVisited);
+					queue.offer(target);
+				}
+			}
+		}
+
+		if(!visited.containsKey(inEnd))
+			return path;
+
+		VisitedNode current = visited.get(inEnd);
+		while(current != null)
+		{
+			path.getVertexes().add(current.name);
+			current = visited.get(current.parent);
+		}
+		Collections.reverse(path.getVertexes());
+		return path;
+	}
+
+	public String toString()
+	{
+		return "Breadth First Search";
+	}
+
+	private static class VisitedNode
+	{
+		public String name;
+		public int value;
+		public String parent;
+	}
+}
