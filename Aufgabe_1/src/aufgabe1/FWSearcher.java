daa61bfa4294694b0b6980e9ad448acf61096e40
Tim Hagemann <tim@infinityblade.de>
2014-11-29 12:53:00 +0100

    Keep matrixes when searching the same graph. Closes #26

--- Aufgabe_1/src/aufgabe1/FWSearcher.java
+++ Aufgabe_1/src/aufgabe1/FWSearcher.java
@@ -11,6 +11,7 @@ public class FWSearcher implements SearchAlgorithm {
 	ArrayList<String> vertices = new ArrayList<>();
 	String vertexToAdd = "";
 	int numberOfVertecis = 0;
+	Graph<String, WeightedNamedEdge> lastGraph;
 	ArrayList<ArrayList<Integer>> matrixD;
 	ArrayList<ArrayList<Integer>> matrixT;
 	
@@ -24,109 +25,11 @@ public class FWSearcher implements SearchAlgorithm {
 		if(!inGraph.containsVertex(inStart) || !inGraph.containsVertex(inEnd)){
 			return path;
 		}
-		
-		//Number of Vertices in the Graph
-		numberOfVertecis = inGraph.vertexSet().size(); 
-		
-		//Add one step
-		path.setSteps(path.getSteps() + 1);
-		
-		//Set -> ArrayList
-		vertices.addAll(inGraph.vertexSet());
-		
-		System.out.println(vertices);
-		System.out.println("----------------------");
-		
-		//Matrix D (2-D Array)
-		matrixD = new ArrayList<>();
-		for(int i=0; i < numberOfVertecis; i++){
-			matrixD.add(i, new ArrayList<Integer>());
-		}
-		
-		//Matrix T (2-D Array)
-		matrixT = new ArrayList<>();
-		for(int i=0; i < numberOfVertecis; i++){
-				matrixT.add(i, new ArrayList<Integer>());
-		}
-		
-		//Filling Matrix D
-		int indexOfArray=0;
-		for(String knoten : vertices){
-			
-			for(int index=0; index < vertices.size(); index++){
-				//Add step
-				path.setSteps(path.getSteps() + 1);
-				
-				int weight;
-				//Distance to your own
-				if(knoten.equals(vertices.get(index))){
-					weight=0;
-				}
-				//there is "no" edge to the vertex
-				else if(inGraph.getAllEdges(knoten, vertices.get(index)).size()==0){
-					weight=Integer.MAX_VALUE;
-				}
-				//there is an edge to the vertex
-				else{
-					weight = inGraph.getEdge(knoten, vertices.get(index)).getWeigth();
-				}
-				matrixD.get(indexOfArray).add(index, weight);
-			}
-			indexOfArray++;
-		}
-		
-		//Filling Matrix T (with "-1")
-		for(ArrayList<Integer> array: matrixT){
-			for(int i=0; i < vertices.size(); i++){
-				array.add(-1);
-			}
-		}
-		
-		//Print matrix D in Console
-		for(int i=0; i < matrixD.size(); i++){
-			System.out.println(matrixD.get(i));
-		}
-		
-		System.out.println("----------------------");
-		
-		//Print matrix T in Console
-		for(int i=0; i < matrixD.size(); i++){
-			System.out.println(matrixT.get(i));
-		}
-		
-		//Algorithms
-		for(int j=0; j < vertices.size(); j++ ){
-			for(int i=0; i < vertices.size(); i++){
-				//i and j have to be different
-				if(i!=j){
-					for(int k=0; k < vertices.size(); k++){
-						//k and j have to be different
-						if(k!=j){
-							int temp_ik = matrixD.get(i).get(k);
-							int temp_ij = matrixD.get(i).get(j);
-							int temp_jk = matrixD.get(j).get(k);
-							if(temp_ij < Integer.MAX_VALUE && temp_jk < Integer.MAX_VALUE)
-							{
-								//get minimum
-								int minimum = Math.min(temp_ik, (temp_ij + temp_jk));
-								if(temp_ik!=minimum){
-									
-									//remove old value
-									matrixD.get(i).remove(k);
-									//add minimum as new value
-									matrixD.get(i).add(k, minimum);
-									//set matrixT_ik
-									matrixT.get(i).remove(k);
-									
-									matrixT.get(i).add(k, j);
-								}
-							}
-							
-						}
-					}
-							
-				}	
-			}		
+
+		if(this.lastGraph == null || !this.lastGraph.equals(inGraph))
+		{
+			this.lastGraph = inGraph;
+			buildMatrix(inGraph);
 		}
 		
 		//indices of Start and End for path finding
@@ -162,10 +65,116 @@ public class FWSearcher implements SearchAlgorithm {
 
 		ShowMatrixT show = new ShowMatrixT(this.getMatrixT());
 		show.setVisible(true);
-
 		return path;
 	}
-	
+
+	private void buildMatrix(final Graph<String, WeightedNamedEdge> inGraph)
+	{
+		//Number of Vertices in the Graph
+		numberOfVertecis = inGraph.vertexSet().size();
+
+		//Add one step
+		path.setSteps(path.getSteps() + 1);
+
+		//Set -> ArrayList
+		vertices.addAll(inGraph.vertexSet());
+
+		System.out.println(vertices);
+		System.out.println("----------------------");
+
+		//Matrix D (2-D Array)
+		matrixD = new ArrayList<>();
+		for(int i=0; i < numberOfVertecis; i++){
+			matrixD.add(i, new ArrayList<Integer>());
+		}
+
+		//Matrix T (2-D Array)
+		matrixT = new ArrayList<>();
+		for(int i=0; i < numberOfVertecis; i++){
+			matrixT.add(i, new ArrayList<Integer>());
+		}
+
+		//Filling Matrix D
+		int indexOfArray=0;
+		for(String knoten : vertices){
+
+			for(int index=0; index < vertices.size(); index++){
+				//Add step
+				path.setSteps(path.getSteps() + 1);
+
+				int weight;
+				//Distance to your own
+				if(knoten.equals(vertices.get(index))){
+					weight=0;
+				}
+				//there is "no" edge to the vertex
+				else if(inGraph.getAllEdges(knoten, vertices.get(index)).size()==0){
+					weight=Integer.MAX_VALUE;
+				}
+				//there is an edge to the vertex
+				else{
+					weight = inGraph.getEdge(knoten, vertices.get(index)).getWeigth();
+				}
+				matrixD.get(indexOfArray).add(index, weight);
+			}
+			indexOfArray++;
+		}
+
+		//Filling Matrix T (with "-1")
+		for(ArrayList<Integer> array: matrixT){
+			for(int i=0; i < vertices.size(); i++){
+				array.add(-1);
+			}
+		}
+
+		//Print matrix D in Console
+		for(int i=0; i < matrixD.size(); i++){
+			System.out.println(matrixD.get(i));
+		}
+
+		System.out.println("----------------------");
+
+		//Print matrix T in Console
+		for(int i=0; i < matrixD.size(); i++){
+			System.out.println(matrixT.get(i));
+		}
+
+		//Algorithms
+		for(int j=0; j < vertices.size(); j++ ){
+			for(int i=0; i < vertices.size(); i++){
+				//i and j have to be different
+				if(i!=j){
+					for(int k=0; k < vertices.size(); k++){
+						//k and j have to be different
+						if(k!=j){
+							int temp_ik = matrixD.get(i).get(k);
+							int temp_ij = matrixD.get(i).get(j);
+							int temp_jk = matrixD.get(j).get(k);
+							if(temp_ij < Integer.MAX_VALUE && temp_jk < Integer.MAX_VALUE)
+							{
+								//get minimum
+								int minimum = Math.min(temp_ik, (temp_ij + temp_jk));
+								if(temp_ik!=minimum){
+
+									//remove old value
+									matrixD.get(i).remove(k);
+									//add minimum as new value
+									matrixD.get(i).add(k, minimum);
+									//set matrixT_ik
+									matrixT.get(i).remove(k);
+
+									matrixT.get(i).add(k, j);
+								}
+							}
+
+						}
+					}
+
+				}
+			}
+		}
+	}
+
 	//building the Path, checking recursive
 	public List<String> buildPath(List<String> inList){
 		int first = vertices.indexOf(inList.get(0));
@@ -210,17 +219,18 @@ public class FWSearcher implements SearchAlgorithm {
 		System.out.println("Matrix T:");
 		
 		//plus 1, because "internal" sight is -1
-		for(ArrayList<Integer> list : matrixT){
-			for(int z=0; z < list.size(); z++){
-				Integer zahl = list.get(z);
-					list.remove(z);
-					list.add(z, (zahl+1));
+		for(ArrayList<Integer> aMatrixT : matrixT)
+		{
+			List<Integer> copy = new ArrayList<>(aMatrixT);
+			for(int z = 0; z < copy.size(); z++)
+			{
+				Integer zahl = copy.get(z);
+				copy.remove(z);
+				copy.add(z, (zahl + 1));
 			}
+			System.out.println(aMatrixT);
 		}
-		
-		for(int i=0; i < matrixT.size(); i++){
-			System.out.println(matrixT.get(i));
-		}
+
 		System.out.println("------------------------");
 	}
 
