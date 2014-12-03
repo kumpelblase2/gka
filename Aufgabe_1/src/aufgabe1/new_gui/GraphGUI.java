cef33f39a58d009f372b124fd592ae2203f896d0
Tim Hagemann <tim@infinityblade.de>
2014-12-03 20:15:22 +0100

    Add edmond karp to algorithm list

--- Aufgabe_1/src/aufgabe1/new_gui/GraphGUI.java
+++ Aufgabe_1/src/aufgabe1/new_gui/GraphGUI.java
@@ -78,6 +78,7 @@ public class GraphGUI
 		this.m_searchSelection.addItem(new DijkstraSearcher());
 		this.m_searchSelection.addItem(new FWSearcher());
 		this.m_searchSelection.addItem(new FordFulkerson());
+		this.m_searchSelection.addItem(new EdmondsKarp());
 
 		this.m_searchSelection.addItemListener(new ItemListener()
 		{
