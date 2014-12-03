0843b266a60ae4ce9fb8bd258f3699a3eed7e098
sebdied <sebastian.diedrich@t-online.de>
2014-12-03 12:20:09 +0100

    Added zeitmessung

---
+++ Aufgabe_1/test/aufgabe1/Zeitmessung.java
@@ -0,0 +1,71 @@
+package aufgabe1;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Date;
+
+import org.jgrapht.Graph;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import aufgabe1.EdmondsKarp;
+import aufgabe1.FordFulkerson;
+import aufgabe1.GraphParser;
+
+
+public class Zeitmessung {
+
+	public static Graph graph;
+	
+	@BeforeClass
+	public static void setUp() throws Exception {
+		File file = new File("./resources/graph16(ford_fulk).gka");
+		graph = GraphParser.parse(file);
+	}
+
+	@Test
+	public void test() {
+		long akkuFord = 0;
+		long akkuEdmond = 0;
+		
+		long timeFord;
+		long diffFord;
+		
+		long timeEdmond;
+		long diffEdmond;
+		
+		for(int i = 0; i < 100; i++)
+		{
+		
+		FordFulkerson ford = new FordFulkerson();
+		timeFord = System.currentTimeMillis();
+		ford.search(graph, "q", "s");
+		diffFord = System.currentTimeMillis() - timeFord;
+		akkuFord = akkuFord + diffFord;
+		System.out.println("Ford: "+diffFord);
+		
+		EdmondsKarp edmond = new EdmondsKarp();
+		timeEdmond = System.currentTimeMillis();
+		edmond.search(graph, "q", "s");
+		diffEdmond = System.currentTimeMillis() - timeEdmond;
+		akkuEdmond = akkuEdmond + diffEdmond;
+		System.out.println("Edmond: "+diffEdmond);
+	    }
+		
+		System.out.println("Akku Ford: "+akkuFord);
+		System.out.println("Akku Edmond: "+akkuEdmond);
+		if(akkuFord < akkuEdmond){
+			System.out.println("Ford is faster");
+		}	
+		else
+		{
+			System.out.println("Edmond is faster"+"("+(int)((((double)akkuEdmond)/((double)akkuFord))*100)+"%)");
+		}	
+			System.out.flush();
+	}
+	
+	
+	
+
+}
