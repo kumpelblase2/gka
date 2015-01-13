34804fafd5445fefa2f5c5b135302302f3ccc725
Tim Hagemann <tim@infinityblade.de>
2015-01-13 10:37:10 +0100

    Remove try

--- Aufgabe_1/test/aufgabe1/GraphGeneratorTest.java
+++ Aufgabe_1/test/aufgabe1/GraphGeneratorTest.java
@@ -49,23 +49,16 @@ public class GraphGeneratorTest
 
 	public void testGenerateMetric()
 	{
-		try
-		{
-			GraphGenerator.GeneratorProperties properties = new GraphGenerator.GeneratorProperties(MIN_EDGES, MAX_EDGES, MIN_VERTEXES, MAX_VERTEXES, DIRECTED, MIN_WEIGHT, MAX_WEIGHT);
-			Graph<String, WeightedNamedEdge> generated = GraphGenerator.generate(properties);
+		GraphGenerator.GeneratorProperties properties = new GraphGenerator.GeneratorProperties(MIN_EDGES, MAX_EDGES, MIN_VERTEXES, MAX_VERTEXES, DIRECTED, MIN_WEIGHT, MAX_WEIGHT);
+		Graph<String, WeightedNamedEdge> generated = GraphGenerator.generate(properties);
 
-			Assert.assertTrue("Not right vertex size: " + generated.vertexSet().size() + " min: " + MIN_VERTEXES + " max: " + MAX_VERTEXES, generated.vertexSet().size() <= properties.maxVertexes && generated.vertexSet().size() >= properties.minVertexes);
-			Assert.assertTrue("Not right edge size: " + generated.edgeSet().size() + " min: " + MIN_EDGES + " max: " + MAX_EDGES, generated.edgeSet().size() <= properties.maxEdges && generated.edgeSet().size() >= properties.minEdges);
-			Assert.assertTrue(generated instanceof DirectedGraph);
-			for(WeightedNamedEdge edge : generated.edgeSet())
-			{
-				Assert.assertTrue(edge.hasWeigth());
-				Assert.assertTrue("Not right weight: " + edge.getWeigth() + " min: " + MIN_WEIGHT + " max: " + MAX_WEIGHT, edge.getWeigth() >= properties.minWeight && edge.getWeigth() <= properties.maxWeight);
-			}
-		}
-		catch(Exception e)
+		Assert.assertTrue("Not right vertex size: " + generated.vertexSet().size() + " min: " + MIN_VERTEXES + " max: " + MAX_VERTEXES, generated.vertexSet().size() <= properties.maxVertexes && generated.vertexSet().size() >= properties.minVertexes);
+		Assert.assertTrue("Not right edge size: " + generated.edgeSet().size() + " min: " + MIN_EDGES + " max: " + MAX_EDGES, generated.edgeSet().size() <= properties.maxEdges && generated.edgeSet().size() >= properties.minEdges);
+		Assert.assertTrue(generated instanceof DirectedGraph);
+		for(WeightedNamedEdge edge : generated.edgeSet())
 		{
-			Assert.fail("Couldn't generate metric graph.");
+			Assert.assertTrue(edge.hasWeigth());
+			Assert.assertTrue("Not right weight: " + edge.getWeigth() + " min: " + MIN_WEIGHT + " max: " + MAX_WEIGHT, edge.getWeigth() >= properties.minWeight && edge.getWeigth() <= properties.maxWeight);
 		}
 	}
 
